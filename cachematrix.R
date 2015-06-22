a13621ebb871206c1a2d4254259f1092715f8d47
RLDGG <ricardo.delagarza@gmail.com>
2015-06-21 19:49:37 -0500

    Ejercicio2d

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function allows to create a matrix and print it also, it also allow to see if the 
+## matrix has an inverse and we can set the inverse also.
 
 makeCacheMatrix <- function(x = matrix()) {
-#Ejemplo de como hacer repo push
+  makeCacheMatrix <- function(x = matrix()) {
+    ## return: a list containing functions to
+    ##              1. set the matrix
+    ##              2. get the matrix
+    ##              3. set the inverse
+    ##              4. get the inverse
+    ##         this list is used as the input to cacheSolve()
+    
+    inv = NULL
+    set = function(y) {
+      # use `<<-` to assign a value to an object in an environment 
+      # different from the current environment. 
+      x <<- y
+      inv <<- NULL
+    }
+    get = function() x
+    setinv = function(inverse) inv <<- inverse 
+    getinv = function() inv
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
+  }
+  
 }
 
 
@@ -12,4 +31,25 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## @x: output of makeCacheMatrix()
+  ## return: inverse of the original matrix input to makeCacheMatrix()
+  
+  inv = x$getinv()
+  
+  # if the inverse has already been calculated
+  if (!is.null(inv)){
+    # get it from the cache and skips the computation. 
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # otherwise, calculates the inverse 
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  # sets the value of the inverse in the cache via the setinv function.
+  x$setinv(inv)
+  
+  return(inv)
+
 }
